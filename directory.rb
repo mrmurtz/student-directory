d24a4f0d36141dea0701bd864f2d8645a38ec5a1
mrmurtz <murtaza.s.abidi@gmail.com>
2016-08-03 11:56:18 +0100

    Added iteration on student array and removed count variable

--- directory.rb
+++ directory.rb
@@ -1,19 +1,25 @@
-student_count = 11
+# Put students into an array
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Fredy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
 
 # Print list of students
-puts "The students of Villans Academy"
-puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+puts
+puts "The students of Villains Academy"
+puts "---------------"
+students.each do |student|
+  puts student
+end
 
 #Print student count
-puts "Overall we have #{student_count} great students."
+puts "Overall we have #{students.count} great students."
