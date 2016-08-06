cdedf6dad55ddc1fa5456ee4ebb7ea84f59aa86e
mrmurtz <murtaza.s.abidi@gmail.com>
2016-08-06 18:48:53 +0100

    Exercise 8.4 changing each method to while

--- directory.rb
+++ directory.rb
@@ -5,15 +5,17 @@ def input_students
   #getting the students name in a variable
   name = gets.chomp
   while !name.empty? do
+    #ask for cohort
+    puts "Which cohort will they be enrolled?"
+    cohort = gets.chomp
 
     # ask for which country they are from
     puts "Which country are they from?"
     country = gets.chomp
-
     # ask for hobbies
     puts "Any hobbies?"
     hobby = gets.chomp
-    students << {name: name, cohort: :november, countries: country, hobbies: hobby}
+    students << {name: name, cohort: cohort, countries: country, hobbies: hobby}
     puts "Now we have #{students.count} students."
     puts "Add another name (or hit enter twice to exit)"
     # get another name
@@ -32,20 +34,20 @@ def print_header
 end
 
 #Using each to print students with index also Capitalize puts of name
-def print(students)
- students.each_with_index do | student, index |
-   puts "#{index+1}. #{student[:name].capitalize} (#{student[:cohort].capitalize}'s cohort) from #{student[:countries].capitalize}, Enjoys #{student[:hobbies]}"
- end
-end
-
-# Can't get while to work for excercise 8.4
 # def print(students)
-# 		index = 0
-# 		while students.length >= index+1
-# 			puts "#{index+1}. #{students[:name]} (#{students[:cohort]} cohort)"
-# 			index+=1
-# 		end
+#  students.each_with_index do | student, index |
+#    puts "#{index+1}. #{student[:name].capitalize} (#{student[:cohort].capitalize} cohort) from #{student[:countries].capitalize}, Enjoys #{student[:hobbies]}"
+#  end
 # end
+
+# C [index][:cohort} should work, as then you’re directing it to a certain hash within the array
+def print(students)
+		index = 0
+		while students.length >= index+1
+			puts "#{index+1}. #{students[index][:name]} (#{students[index][:cohort]} cohort)"
+			index+=1
+		end
+end
 
 
 #Method to print footer with count
