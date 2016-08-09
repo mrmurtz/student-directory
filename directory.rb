c8e929e485f0ecf4d66ecf53cebd6b3f3e09c22c
mrmurtz <murtaza.s.abidi@gmail.com>
2016-08-09 15:31:33 +0200

    Exercise 8.9 singular and plural using if else statement

--- directory.rb
+++ directory.rb
@@ -21,7 +21,12 @@ def input_students
 
     # adding the student hash to the array
     students << {name: name, cohort: cohort}
-    puts "Now we have #{students.count} students."
+    if students.length <=1
+      puts "Now we have #{students.count} student."
+    else
+      puts "Now we have #{students.count} students."
+    end
+
     puts "Add another name (or hit enter twice to exit)"
     # get another name
     name = gets.chomp
